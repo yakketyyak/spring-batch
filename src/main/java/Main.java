eee573cf59a49fd0026da456c5a58f68d7bffc36
Patrick BEUGRE <pt.beugre@gmail.com>
2019-08-04 13:47:19 +0200

    add java config for job

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,18 @@
+import config.BatchConfig;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     public static void main(String args[]){
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("job/job-config.xml");
+        /*ApplicationContext context = new ClassPathXmlApplicationContext("job/job-config.xml");
 
         JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
         Job job = (Job) context.getBean("myFirstJob");
@@ -21,6 +26,28 @@ public class Main {
 
         } catch (JobExecutionException e) {
             System.out.println("Job ExamResult failed");
+            e.printStackTrace();
+        }*/
+        ApplicationContext context = new AnnotationConfigApplicationContext(BatchConfig.class);
+
+        JobLauncher jobLauncher = context.getBean(JobLauncher.class);
+        Job job = context.getBean("listUserJob", Job.class);
+
+        JobParameters jobParameters = new JobParametersBuilder().toJobParameters();
+
+        try {
+            JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+        }
+        catch (JobExecutionAlreadyRunningException e) {
+            e.printStackTrace();
+        }
+        catch (JobRestartException e) {
+            e.printStackTrace();
+        }
+        catch (JobInstanceAlreadyCompleteException e) {
+            e.printStackTrace();
+        }
+        catch (JobParametersInvalidException e) {
             e.printStackTrace();
         }
     }
---
+++ src/main/java/config/BatchConfig.java
@@ -0,0 +1,155 @@
+package config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import domain.User;
+import infra.UserRowMapper;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
+import org.springframework.batch.item.*;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+
+    @Bean
+    public NamedParameterJdbcTemplate getTemplate() throws Exception{
+        return new NamedParameterJdbcTemplate(getDataSource());
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new ResourcelessTransactionManager();
+    }
+
+    @Bean
+    public JobRepository jobRepository() throws Exception {
+        return new MapJobRepositoryFactoryBean(transactionManager()).getJobRepository();
+    }
+
+    @Bean
+    public JobLauncher jobLauncher() throws Exception {
+        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+        jobLauncher.setJobRepository(jobRepository());
+        return jobLauncher;
+    }
+    @Bean
+    public Step step1() throws Exception{
+        return stepBuilderFactory.get("step1")
+                .<List<User>, List<User>>chunk(2)
+                .reader(userReader())
+                .processor(userProcessor())
+                .writer(userWriter())
+                .build();
+    }
+
+    @Bean
+    public Job listUserJob(Step step1) throws Exception {
+        return jobBuilderFactory.get("listUserJob")
+                .start(step1())
+                .build();
+    }
+    @Bean
+    ItemReader<List<User>> userReader(){
+        return new ItemReader<List<User>>() {
+            @Override
+            public List<User> read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+                String QUERY = "SELECT * FROM user";
+                List<User> users = getTemplate().query(QUERY, new UserRowMapper());
+                users.forEach(System.out::println);
+                return users;
+            }
+        };
+
+    }
+    @Bean
+    ItemProcessor<List<User>,List<User>> userProcessor(){
+        return new ItemProcessor<List<User>, List<User>>() {
+            @Override
+            public List<User> process(List<User> users) throws Exception {
+                System.out.println("PROCESSOR");
+                users.stream().forEach(user-> {
+                    ((User) user).setExpired(new Date());
+                    ((User) user).setLocked(Boolean.FALSE);
+                    user.setForgotPasswordCode(new String("JESUS"));
+                });
+                return users;
+            }
+        };
+    }
+
+    @Bean
+    ItemWriter<List<User>> userWriter() throws Exception{
+        return new ItemWriter<List<User>>() {
+            @Override
+            public void write(List<? extends List<User>> users) throws Exception {
+                System.out.println("WRITER");
+                users.forEach(System.out::println);
+                String QUERY = "UPDATE user set expired = :expired, locked = :locked, forgot_password_code = :forgot_password_code where id = :id";
+                users.forEach(user->{
+                    user.forEach(u->{
+                        MapSqlParameterSource in = new MapSqlParameterSource();
+                        in.addValue("id", u.getId());
+                        in.addValue("expired", u.getExpired());
+                        in.addValue("locked", u.getLocked());
+                        in.addValue("forgot_password_code", u.getForgotPasswordCode());
+                        try {
+                            getTemplate().update(QUERY, in);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
+
+                });
+            }
+        };
+    }
+
+    @Bean(name="dataSource")
+    public DataSource getDataSource() throws IOException {
+        Resource resource = new ClassPathResource("/db.properties");
+        Properties props = PropertiesLoaderUtils.loadProperties(resource);
+        HikariConfig hc = new HikariConfig();
+        hc.setDriverClassName(props.getProperty("driverClassName"));
+        hc.setJdbcUrl(props.getProperty("jdbcUrl"));
+        hc.setUsername(props.getProperty("username"));
+        hc.setPassword(props.getProperty("password"));
+        hc.setPoolName("PoolName");
+
+        return new HikariDataSource(hc);
+    }
+}
